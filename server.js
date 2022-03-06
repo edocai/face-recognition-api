b0f620b107a7786bdb368c3186ab8885f6117890
edocai <edocaai@hotmail.com>
2022-03-06 17:05:29 +0300

    updating database

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE.URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 });
