05956cda7f533447e406018742170a75e7ea4265
edocai <edocaai@hotmail.com>
2022-03-06 17:02:48 +0300

    updating database

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        port: 5432,
-        user: 'postgres',
-        password: 'test',
-        database: 'smart-brain'
+        connectionString: process.env.DATABASE.URL,
+        ssl: true
     }
 });
 
